164838622a8b67a8bdc0e6ee9cc19c5774e008b8
Olha Halat <olha.halat@gmail.com>
2020-08-02 15:02:31 +0200

    replace App function component with App class component, replace hooks with states

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Clock from "./clock/Clock";
 import Counter from "./counter/Counter";
@@ -32,95 +32,118 @@ const comment = {
   }
 
 
-const App = () => {
-  const [isClassTopicsVisible, setIsClassTopicsVisible] = useState(false);
-  const [isClockVisible, setIsClockVisible] = useState(false);
-  const [isCounterVisible, setIsCounterVisible] = useState(false);
-  const [isCommentVisible, setIsCommentVisible] = useState(false);
-  const [isCalculatorVisible, setIsCalculatorVisible] = useState(false);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClassTopicsVisible: false,
+      isClockVisible: false,
+      isCounterVisible: false,
+      isCommentVisible: false,
+      isCalculatorVisible: false
+    }
+  }
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({
+              isClassTopicsVisible: true,
+              isClockVisible: false,
+              isCounterVisible: false,
+              isCommentVisible: false,
+              isCalculatorVisible: false
+            });
+          }}
+        >
+          Show Class Topics
+        </button>
 
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setIsClassTopicsVisible(true);
-          setIsClockVisible(false);
-          setIsCounterVisible(false);
-          setIsCommentVisible(false);
-          setIsCalculatorVisible(false);
-        }}
-      >
-        Show Class Topics
-      </button>
-      <button
-        onClick={() => {
-          setIsClassTopicsVisible(false);
-          setIsClockVisible(true);
-          setIsCounterVisible(false);
-          setIsCommentVisible(false);
-          setIsCalculatorVisible(false);
-        }}
-      >
-        Show Clock
-      </button>
-      <button
-        onClick={() => {
-          setIsClassTopicsVisible(false);
-          setIsClockVisible(false);
-          setIsCounterVisible(true);
-          setIsCommentVisible(false);
-          setIsCalculatorVisible(false);
-        }}
-      >
-        Show Counter
-      </button>
-      <button
-        onClick={() => {
-          setIsClassTopicsVisible(false);
-          setIsClockVisible(false);
-          setIsCounterVisible(false);
-          setIsCommentVisible(true);
-          setIsCalculatorVisible(false);
-        }}
-      >
-        Show Comment
-      </button>
-      <button
-        onClick={() => {
-            setIsClassTopicsVisible(false);
-            setIsClockVisible(false);
-            setIsCounterVisible(false);
-            setIsCommentVisible(false);
-            setIsCalculatorVisible(true);
-        }}
-      >
-        Show Calculator
-      </button>
-      {isClassTopicsVisible && <ClassTopics />}
-      {isClockVisible && <Clock />}
-      {isCounterVisible && <Counter />}
-      {isCommentVisible && 
-        <div>
-            <Comment 
-                date={comment.date}
-                text={comment.text}
-                author={comment.author} 
-            />
-            <Comment 
-                date={comment2.date}
-                text={comment2.text}
-                author={comment2.author}
-            />
-            <Comment 
-                date={comment3.date}
-                text={comment3.text}
-                author={comment3.author}
-            />
-        </div>
-      }
-      {isCalculatorVisible && <Calculator />}
-    </div>
-  );
-};
+        <button
+          onClick={() => {
+            this.setState({
+              isClassTopicsVisible: false,
+              isClockVisible: true,
+              isCounterVisible: false,
+              isCommentVisible: false,
+              isCalculatorVisible: false
+            });
+          }}
+        >
+          Show Clock
+        </button>
+
+        <button
+          onClick={() => {
+            this.setState({
+              isClassTopicsVisible: false,
+              isClockVisible: false,
+              isCounterVisible: true,
+              isCommentVisible: false,
+              isCalculatorVisible: false
+            });
+          }}
+        >
+          Show Counter
+        </button>
+
+        <button
+          onClick={() => {
+            this.setState({
+              isClassTopicsVisible: false,
+              isClockVisible: false,
+              isCounterVisible: false,
+              isCommentVisible: true,
+              isCalculatorVisible: false
+            });
+          }}
+        >
+          Show Comments
+        </button>
+
+        <button
+          onClick={() => {
+            this.setState({
+              isClassTopicsVisible: false,
+              isClockVisible: false,
+              isCounterVisible: false,
+              isCommentVisible: false,
+              isCalculatorVisible: true
+            });
+          }}
+        >
+          Show Calculator
+        </button>
+
+        {this.state.isClassTopicsVisible && <ClassTopics />}
+        {this.state.isClockVisible && <Clock />}
+        {this.state.isCounterVisible && <Counter />}
+        {this.state.isCommentVisible && 
+          <div>
+              <Comment 
+                  date={comment.date}
+                  text={comment.text}
+                  author={comment.author} 
+              />
+              <Comment 
+                  date={comment2.date}
+                  text={comment2.text}
+                  author={comment2.author}
+              />
+              <Comment 
+                  date={comment3.date}
+                  text={comment3.text}
+                  author={comment3.author}
+              />
+          </div>
+        }
+        {this.state.isCalculatorVisible && <Calculator />}
+      </div>
+    );
+  }
+
+  
+}
 
 export default App;
